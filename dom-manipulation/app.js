a812851d7131a3e1210ff7fd59644551e988bda1
diken.dev <diken.dev@gmail.com>
2022-10-04 00:05:31 -0300

    added itens to DOM, and Remove

--- dom-manipulation/app.js
+++ dom-manipulation/app.js
@@ -1,34 +1,43 @@
-// // DOm Manipulation
+// DOM Manipulation
 
-
-// // const title = document.querySelector('#main-heading');
-
-// // title.style.color = 'blue';
-
-// const listitems = document.querySelectorAll('li');
-
-
-// for(i = 0; i < listitems.length; i++) {
-//     listitems[i].style.fontSize = '1rem';
-// }
-
-// console.log(listitems);
+// Traverse The DOM
 
 
 
-// creating elements
 
-const ul = document.querySelector('ul');
-const li = document.createElement('li');
+// Parent Node Traversal
 
 
-// adding Elements
-ul.append(li);
+// console.log(ul.parentNode.parentNode);
+// console.log(ul.parentElement.parentElement);
 
-// modifying the text
+// const html = document.documentElement;
+// console.log(html.parentNode)
+// console.log(html.parentElement);
 
-li.innerText = 'X-Men';
+// Child Node Traversal
 
-// Modifying Atttributes & Classes
 
-li.classList.add
+// console.log(ul.childNodes);
+// console.log(ul.firstChild);
+// console.log(ul.lastChild);
+
+// ul.childNodes[2].style.backgroundColor = 'red';
+
+// console.log(ul.children);
+// console.log(ul.firstElementChild);
+// console.log(ul.lastElementChild);
+
+
+
+// Sibing Node Traversal
+
+let ul = document.querySelector('ul');
+const div = document.querySelector('div');
+
+// console.log(div.childNodes);
+
+console.log(ul.previousElementSibling);
+console.log(ul.nextElementSibling);
+
+
